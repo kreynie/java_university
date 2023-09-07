74f69f1ed058d4a16b5c2259194b51903cb2f3c0
kreyNie <kreynie@vk.com>
2023-09-07 15:47:24 +0300

    first practice, ex. 7

--- prj_1/src/Main.java
+++ prj_1/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first creation");
+        System.out.print("Целое число: ");
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            System.out.println("Введено некорректное число");
+            return;
+        }
+        int given_number = sc.nextInt();
+
+
+        if (given_number < 1) {
+            System.out.println("Число должно быть больше единицы");
+            return;
+        }
+        System.out.println(factorial(given_number));
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i < n+1; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
